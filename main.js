e9125cd3fa33f08389957e31e42f9a7bcd507e51
Stefan Carrera <stefancarrera@gmail.com>
2021-09-16 12:31:59 -0500

    prev button functions
    prev button cycles through the comic panels

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ const $panel = document.getElementById('panel');
 let $panelNum = 1;
 
 $next.addEventListener('click', () => {
-  console.log('I hit next, bitch');
   if ($panelNum === 7) {
     $panelNum = 1;
     changePanel();
@@ -12,14 +11,17 @@ $next.addEventListener('click', () => {
     $panelNum++;
     changePanel();
   }
-  console.log('And the panel number is', $panelNum);
 });
 
 $prev.addEventListener('click', () => {
-  console.log('I hit prev, hctib');
-  $panelNum--;
-  console.log('And the panel number is', $panelNum);
-})
+  if ($panelNum === 1) {
+    $panelNum = 7;
+    changePanel();
+  } else if (($panelNum <= 7) && ($panelNum > 1)) {
+    $panelNum--;
+    changePanel();
+  }
+});
 
 function changePanel() {
   $panel.setAttribute('src', `images/${$panelNum}.png`);
