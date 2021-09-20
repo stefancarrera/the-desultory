5d394a1b9cf9559bd26c61fd44a1cbf3bdcd1269
Stefan Carrera <stefancarrera@gmail.com>
2021-09-20 13:33:51 -0500

    panel3 text transition added
    panel3 text transition added

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const $prev = document.getElementById('prev');
 const $panel = document.getElementById('panel');
 const $text1 = document.getElementById('text1');
 const $text2 = document.getElementById('text2');
-// const $navRow = document.getElementById('navRow');
+const $navRow = document.getElementById('navRow');
 
 let $panelNum = 1;
 
@@ -70,7 +70,7 @@ function text() {
     setTimeout(() => {
       $text2.setAttribute('src', 'images/text/text1-2.png')
       $text2.className = 'p1Text2 focus-in';
-    }, 3000)
+    }, 3000);
   }
 
   if ($panelNum === 2) {
@@ -81,6 +81,21 @@ function text() {
     setTimeout(() => {
       $text2.setAttribute('src', 'images/text/text2-2.png')
       $text2.className = 'p1Text2 focus-in';
-    }, 3000)
+    }, 3000);
+  }
+
+  if ($panelNum === 3) {
+    setTimeout(() => {
+      const divText1 = document.createElement('div');
+      divText1.className = 'p3Text1 focus-in';
+      divText1.textContent = 'Evolving';
+      $navRow.appendChild(divText1);
+    }, 2000);
+    setTimeout(() => {
+      const divText2 = document.createElement('div');
+      divText2.className = 'p3Text2 focus-in';
+      divText2.textContent = `It yearns for what it doesn't have. An innate drive to improve, to become better, stronger. Its ancestor's struggle has brought it here. To this moment.`
+      $navRow.appendChild(divText2);
+    }, 3000);
   }
 }
