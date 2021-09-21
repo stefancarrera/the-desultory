c6f9c6c2c117ef8bb8c71c772b9da55e72822a8e
Stefan Carrera <stefancarrera@gmail.com>
2021-09-21 10:13:37 -0500

    panel1-new text transition complete
    panel1 text content is no longer an image but a dom created text content

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 const $next = document.getElementById('next');
 const $prev = document.getElementById('prev');
 const $panel = document.getElementById('panel');
-const $text1 = document.getElementById('text1');
-const $text2 = document.getElementById('text2');
+// const $text1 = document.getElementById('text1');
+// const $text2 = document.getElementById('text2');
 const $divText1 = document.getElementById('divText1');
 const $divText2 = document.getElementById('divText2');
+const $textDiv = document.getElementById('textDiv');
 // const $navRow = document.getElementById('navRow');
 
 
@@ -62,33 +63,39 @@ function changePanel() {
 }
 
 function text() {
-  $text1.setAttribute('src', '');
-  $text2.setAttribute('src', '');
+  // $text1.setAttribute('src', '');
+  // $text2.setAttribute('src', '');
   $divText1.textContent = '';
   $divText2.textContent = '';
+  $textDiv.innerHTML = '';
 
-  setTimeout(() => {
     if ($panelNum === 1) {
       setTimeout(() => {
-        $text1.setAttribute('src', `images/text/text1-1.png`)
+        // $text1.setAttribute('src', `images/text/text1-1.png`)
+        const $text1 = document.createElement('div')
         $text1.className = 'p1Text1 focus-in';
+        $text1.textContent = 'So it begins. This miracle of life. Starting so humbly, but wanting more and more.';
+        $textDiv.appendChild($text1);
       }, 2000);
       setTimeout(() => {
-        $text2.setAttribute('src', 'images/text/text1-2.png')
+        // $text2.setAttribute('src', 'images/text/text1-2.png')
+        const $text2 = document.createElement('div');
         $text2.className = 'p1Text2 focus-in';
+        $text2.textContent = 'Through time and sheer force of will. It grows.'
+        $textDiv.appendChild($text2);
       }, 3000);
     }
 
-    if ($panelNum === 2) {
-      setTimeout(() => {
-        $text1.setAttribute('src', `images/text/text2-1.png`)
-        $text1.className = 'p1Text1 focus-in';
-      }, 2000);
-      setTimeout(() => {
-        $text2.setAttribute('src', 'images/text/text2-2.png')
-        $text2.className = 'p1Text2 focus-in';
-      }, 3000);
-    }
+    // if ($panelNum === 2) {
+    //   setTimeout(() => {
+    //     $text1.setAttribute('src', `images/text/text2-1.png`)
+    //     $text1.className = 'p1Text1 focus-in';
+    //   }, 2000);
+    //   setTimeout(() => {
+    //     $text2.setAttribute('src', 'images/text/text2-2.png')
+    //     $text2.className = 'p1Text2 focus-in';
+    //   }, 3000);
+    // }
 
     if ($panelNum === 3) {
       setTimeout(() => {
@@ -112,5 +119,4 @@ function text() {
       }, 3000);
     }
 
-  }, 2000);
 }
