3a960ac80bc1117bf120f6c8e59d90862296a493
Stefan Carrera <stefancarrera@gmail.com>
2021-09-21 10:25:43 -0500

    updated panel2 text transition
    panel 2 now also uses a dom created element to display the text, removing the image used previously.

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 const $next = document.getElementById('next');
 const $prev = document.getElementById('prev');
 const $panel = document.getElementById('panel');
-// const $text1 = document.getElementById('text1');
-// const $text2 = document.getElementById('text2');
 const $divText1 = document.getElementById('divText1');
 const $divText2 = document.getElementById('divText2');
 const $textDiv = document.getElementById('textDiv');
@@ -63,22 +61,18 @@ function changePanel() {
 }
 
 function text() {
-  // $text1.setAttribute('src', '');
-  // $text2.setAttribute('src', '');
   $divText1.textContent = '';
   $divText2.textContent = '';
   $textDiv.innerHTML = '';
 
     if ($panelNum === 1) {
       setTimeout(() => {
-        // $text1.setAttribute('src', `images/text/text1-1.png`)
         const $text1 = document.createElement('div')
         $text1.className = 'p1Text1 focus-in';
         $text1.textContent = 'So it begins. This miracle of life. Starting so humbly, but wanting more and more.';
         $textDiv.appendChild($text1);
       }, 2000);
       setTimeout(() => {
-        // $text2.setAttribute('src', 'images/text/text1-2.png')
         const $text2 = document.createElement('div');
         $text2.className = 'p1Text2 focus-in';
         $text2.textContent = 'Through time and sheer force of will. It grows.'
@@ -86,16 +80,20 @@ function text() {
       }, 3000);
     }
 
-    // if ($panelNum === 2) {
-    //   setTimeout(() => {
-    //     $text1.setAttribute('src', `images/text/text2-1.png`)
-    //     $text1.className = 'p1Text1 focus-in';
-    //   }, 2000);
-    //   setTimeout(() => {
-    //     $text2.setAttribute('src', 'images/text/text2-2.png')
-    //     $text2.className = 'p1Text2 focus-in';
-    //   }, 3000);
-    // }
+    if ($panelNum === 2) {
+      setTimeout(() => {
+        const $text1 = document.createElement('div');
+        $text1.className = 'p2Text1 focus-in';
+        $text1.textContent = 'Changes';
+        $textDiv.appendChild($text1);
+      }, 2000);
+      setTimeout(() => {
+        const $text2 = document.createElement('div');
+        $text2.className = 'p2Text2 focus-in';
+        $text2.textContent = 'Adapts'
+        $textDiv.appendChild($text2);
+      }, 3000);
+    }
 
     if ($panelNum === 3) {
       setTimeout(() => {
